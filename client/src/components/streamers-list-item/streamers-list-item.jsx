2a393b0b84dab33c0c8339a8b8ee3be815e2cb03
MarcusKordunski <marikord26@gmail.com>
2023-06-26 21:06:04 +0200

    add upvote and downvote buttons; styling

--- client/src/components/streamers-list-item/streamers-list-item.jsx
+++ client/src/components/streamers-list-item/streamers-list-item.jsx
@@ -1,4 +1,5 @@
 import "./streamers-list-item.css";
+import { Button } from "../index"
 
 export default function StreamersListItem({
   id,
@@ -11,9 +12,15 @@ export default function StreamersListItem({
 
   return (
     <div className={"streamer-list-item"}>
-      <img className="streamer-list-item__image" src="https://staticc.sportskeeda.com/editor/2023/02/8d8d8-16775633289121.png" alt="Asmongold" />
-      <h2>{name}</h2>
-      <h3>{platform}</h3>
+      <div className="streamer-list-item__top-content">
+        <img className="streamer-list-item__image" src="https://staticc.sportskeeda.com/editor/2023/02/8d8d8-16775633289121.png" alt="Asmongold" />
+        <h2>{name}</h2>
+        <h3>{platform}</h3>
+      </div>
+      <div className="streamer-list-item__bottom-content">
+        <Button className="streamer-list-item__button upvote" value="Upvote" />
+        <Button className="streamer-list-item__button downvote" value="Downvote" />
+      </div>
     </div>
   );
 }
